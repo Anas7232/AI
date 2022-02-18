cc953d4291370af957a88bb9a141f30f1ee621d7
Muhammad Anas <anasali7232@gmail.com>
2022-02-18 19:32:43 +0500

    openai

---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png';
+import shopify from '../../assets/shopify.png';
+import dropbox from '../../assets/dropbox.png';
+import atlassian from '../../assets/atlassian.png';
+import slack from '../../assets/slack.png';
+
+export {
+    google,
+    shopify,
+    dropbox,
+     atlassian,
+    slack
+}
